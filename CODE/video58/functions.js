8bbfb6c5699adf3e0c69a628a5135ae924aa9ff6
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-09 20:25:55 +0530

    Functions Intro Completed

---
+++ CODE/video58/functions.js
@@ -0,0 +1,8 @@
+function naming_person(name) {
+  console.log("This boy named " + name + " is very good")
+  console.log(name + "'s handwritting is very good")
+  console.log("Roll number 24 is " + name)
+  
+}
+
+naming_person("Krishna")
