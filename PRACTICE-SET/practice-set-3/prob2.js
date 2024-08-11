bf8de80ae50fd1850e5637175b1c035d3f52fdac
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-11 14:57:32 +0530

    Problem 2 Completed

---
+++ PRACTICE-SET/practice-set-3/prob2.js
@@ -0,0 +1,19 @@
+const correct_num = 9
+
+
+
+
+while (true) {
+  output = prompt("enter>>")
+  if (output == correct_num) {
+    console.log("Correct Number")
+    break
+    
+  }
+
+  else{
+    console.log("Try again")
+  }
+  
+}
+
