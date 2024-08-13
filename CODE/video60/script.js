48565ecada15a8bb004b1fa73ffaeb3e12622472
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-13 18:28:06 +0530

    Completed Video60 Basics

---
+++ CODE/video60/script.js
@@ -0,0 +1,9 @@
+let real_name = "Krishna";
+let friend = "no_one"
+
+
+console.log(`My name is ${real_name} and my friend's name is ${friend}`)
+console.log("Hela\njsw")
+
+
+console.log(friend.toUpperCase())
