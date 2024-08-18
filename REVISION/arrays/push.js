190546322985d770e152d53954957b47f92c8e88
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-18 13:04:52 +0530

    completed push

---
+++ REVISION/arrays/push.js
@@ -0,0 +1,9 @@
+let a = [1,7,6,8,9]
+
+s = 9;
+        // first number is: starting number
+        // second number: how many to be deleted
+        // third,fourth numbers: what numbers to be added
+        
+a.splice(2,3,17,18);
+console.log(a)
