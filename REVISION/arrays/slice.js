c88a26994ba15a7c500b6c3115bdc7bf32ea5dab
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-19 12:42:46 +0530

    Completed Slice

---
+++ REVISION/arrays/slice.js
@@ -0,0 +1,9 @@
+
+//   0 1 2 3 4  --> INDEXING OF THE ARRAY
+a = [1,2,3,4,5]
+
+//slice, slices the array to make a new array with starting and 
+//ending points
+//      i n        i = includes, n=not includes
+s = a.slice(1,3)
+console.log(s)
