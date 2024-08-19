e330c4479b74b26fd6d14954ef83f60a4a81950d
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-19 12:37:33 +0530

    Add  Shift-UnShift-Array

---
+++ REVISION/arrays/shift.js
@@ -0,0 +1,6 @@
+a = [1,2,3,4,5]  //removes the first element of the array
+
+
+a.shift()
+
+console.log(a)
---
+++ REVISION/arrays/unshift.js
@@ -0,0 +1,5 @@
+let a =[1,2,3,4,5]  //Adds Element at the front of the array
+s = 10;
+a.unshift(s);
+
+console.log(a)
