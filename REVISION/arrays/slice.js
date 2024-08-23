d53f684116b2302cda213f904993138f30d7eb46
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-23 14:26:16 +0530

    Update

--- REVISION/arrays/slice.js
+++ REVISION/arrays/slice.js
@@ -5,5 +5,7 @@ a = [1,2,3,4,5]
 //slice, slices the array to make a new array with starting and 
 //ending points
 //      i n        i = includes, n=not includes
-s = a.slice(1,3)
-console.log(s)
+s = a.slice(1,3);
+
+
+console.log(s);
