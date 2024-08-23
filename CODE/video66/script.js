82062f8c4c26be1103e15dd8b9067a906c1bc65a
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-23 14:59:42 +0530

    Completed Vid 66

--- CODE/video66/script.js
+++ CODE/video66/script.js
@@ -1,1 +1,14 @@
-document.title = "Code Check"
+let a = prompt("Enter>> ");
+
+
+
+if (a > 10) {
+  document.body.style.backgroundColor = "green";
+  document.body.style.containerName = "helo";
+}
+
+else if (a < 10) {
+  document.body.style.backgroundColor = "red";
+
+
+}
