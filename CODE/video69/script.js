4a08bd377fe7ce61bc4d13d2f0fa19dd94a30587
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-25 19:16:37 +0530

    Update

---
+++ CODE/video69/script.js
@@ -0,0 +1,13 @@
+let a = prompt(">>");
+function factorial(number) {
+
+  let arr = Array.from(Array(number+1).keys())
+  let c = arr.slice(1,).reduce((a,b)=>{
+    return a*b
+  })
+
+  console.log(c)
+  
+}
+
+factorial(a);
